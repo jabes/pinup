c8f90d79722ca160e79d27f6f1a448f8dc538efb
Justin Bull <bull.justin@gmail.com>
2017-09-20 13:36:36 -0700

    Install sendmail package

--- provision.sh
+++ provision.sh
@@ -7,7 +7,7 @@ sudo apt-get -y update
 # This project is old.. and is not able to use the mysqli extension
 # So downgrade to php5.6
 sudo apt-get -y purge php.*
-sudo apt-get -y install php5.6 php5.6-cli php5.6-common php5.6-curl php5.6-dev php5.6-fpm php5.6-json php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-opcache libapache2-mod-php5.6
+sudo apt-get -y install php5.6 php5.6-cli php5.6-common php5.6-curl php5.6-dev php5.6-fpm php5.6-json php5.6-mbstring php5.6-memcache php5.6-mysql libapache2-mod-php5.6 sendmail
 sudo apt-get -y autoremove
 sudo service apache2 restart
 
